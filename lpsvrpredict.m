e8f94227a53299466dcbb2cbe1569fb6f95318e9
aboutpatcode <aboutpatcode@gmail.com>
2011-11-30 00:49:45 +0000

    lpsvrpredict

---
+++ lpsvrpredict.m
@@ -0,0 +1,26 @@
+% Linear Proximal SVR predict
+%
+% T=lpsvrpredict(P,model)
+%
+% T=A predicted output Nx1
+% P=Input patterns Nxm
+% model=A trained model
+%
+% Copyright by Pat Taweewat
+% in R-Machine
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+function T=lpsvrpredict(P,model)
+T=P*model.w+model.r;
+end
